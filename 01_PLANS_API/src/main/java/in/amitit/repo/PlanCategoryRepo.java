bada373fb2f149cb555ab3f2c1f5e54513d66248
kumarmit2496-cyber <kumarmit.2496@gmail.com>
2025-11-15 17:08:22 +0530

    Delete 01_PLANS_API directory

--- 01_PLANS_API/src/main/java/in/amitit/repo/PlanCategoryRepo.java
+++
@@ -1,9 +0,0 @@
-package in.amitit.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import in.amitit.entity.PlanCategory;
-
-public interface PlanCategoryRepo  extends  JpaRepository<PlanCategory,Integer> {
-
-}
--- 01_PLANS_API/src/main/java/in/amitit/repo/PlanRepo.java
+++
@@ -1,9 +0,0 @@
-package in.amitit.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import in.amitit.entity.Plan;
-
-public interface PlanRepo extends JpaRepository<Plan, Integer> {
-
-}
